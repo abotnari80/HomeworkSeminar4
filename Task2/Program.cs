c21dde890cfc5a8b60f2b9d5741d8c465363183a
abotnari80 <abotnari80@gmail.com>
2022-03-27 17:44:59 +0300

    Задание 2

---
+++ Task2/Program.cs
@@ -0,0 +1,15 @@
+﻿int sum(int A)
+{     
+    int result = 0;
+    for (int i = A; i > 0; i = i / 10)
+    {
+        result = result + (A % 10);
+        A = A / 10;
+    }
+    return result;
+}
+
+System.Console.WriteLine("Введите число ");
+int value = Convert.ToInt32(Console.ReadLine());
+int answer = sum(value);
+System.Console.WriteLine($"Сумма: {answer}");
