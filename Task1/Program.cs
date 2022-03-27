15573773843f3a892fb0b776f0ac8d26575318d4
abotnari80 <abotnari80@gmail.com>
2022-03-27 17:44:49 +0300

    Задание 1

---
+++ Task1/Program.cs
@@ -0,0 +1,16 @@
+﻿int degree(int num1, int num2)
+{
+    int result = num1;
+    for (int i = 1; i < num2; i++)
+    {
+        result = result * num1;
+    }
+    return result;
+}
+
+System.Console.WriteLine("Введите число A ");
+int A = Convert.ToInt32(Console.ReadLine());
+System.Console.WriteLine("Введите число B ");
+int B = Convert.ToInt32(Console.ReadLine());
+int answer = degree(A,B);
+System.Console.WriteLine($"Ответ {answer}");
